0d5d2a37da3c5beb8309f4a70faa77165a83f500
Kevin Fröhlich <oldie8@chello.at>
2026-07-25 21:22:52 +0200

    wrote the logic to play one round of the game

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello, World!")
+const choices = [ "rock", "paper", "scissors"]
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+function getHumanChoice() {
+
+    let humanChoice = prompt("Enter rock, paper, or scissors").toLowerCase()
+    return humanChoice
+
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, it's a draw.`)
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you lose.`)
+        computerScore++
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you win.`)
+        humanScore++
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you win.`)
+        humanScore++
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you lose.`)
+        computerScore++
+    }
+    console.log(`Your Score:${humanScore} Computer Score:${computerScore}`)
+}
+
+playRound(getHumanChoice(), getComputerChoice())
