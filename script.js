e14cef22cecffc3078764b06f35ee61a364a83c3
Kevin Fröhlich <oldie8@chello.at>
2026-08-23 20:05:02 +0200

    Refactored the code to have a GUI

--- script.js
+++ script.js
@@ -1,43 +1,96 @@
+
+
+const container = document.querySelector(".container")
+const startBtn = document.createElement("button")
+startBtn.textContent = "Start Game"
+startBtn.addEventListener( "click", playGame)
+container.appendChild(startBtn)
+
 function playGame() {
+
 const choices = [ "rock", "paper", "scissors"]
+
 let humanScore = 0
 let computerScore = 0
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
-}
+container.innerHTML = ""
+const scores = document.createElement("h1")
+scores.textContent = `Your Score:${humanScore} Computer Score: ${computerScore}`
+const roundWinMessage = document.createElement("h1")
 
-function getHumanChoice() {
+    let humanChoice = ""
+    let computerChoice = ""
 
-    let humanChoice = prompt("Enter rock, paper, or scissors").toLowerCase()
-    return humanChoice
+    const rockBtn = document.createElement("button")
+    rockBtn.textContent = "Rock"
+    rockBtn.addEventListener("click", () => {
+        console.log("clicked")
+        setHumanChoice()
+        console.log(humanChoice)
+        getComputerChoice()
+        console.log(computerChoice)
+        findWinner()
+     })
 
-}
+    const scissorsBtn = document.createElement("button")
+    scissorsBtn.textContent = "Scissors"
+    scissorsBtn.addEventListener("click", () => {
+        setHumanChoice()
+        getComputerChoice()
+        findWinner()
+    })
 
-function playRound(humanChoice, computerChoice) {
+    const paperBtn = document.createElement("button")
+    paperBtn.textContent = "Paper"
+    paperBtn.addEventListener("click", () => {
+        setHumanChoice()
+        getComputerChoice()
+        findWinner()
+     })
+
+    container.append(rockBtn, scissorsBtn, paperBtn, scores)
+
+function findWinner() {
+
     if (humanChoice === computerChoice) {
-        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, it's a draw.`)
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you lose.`)
+        roundWinMessage.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}, it's a draw.`
+    } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "scissors") ) {
+        roundWinMessage.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}, you lose.`
         computerScore++
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you win.`)
+    } else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "rock" && computerChoice === "scissors")) {
+        roundWinMessage.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}, you win.`
         humanScore++
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you win.`)
-        humanScore++
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, you lose.`)
-        computerScore++
     }
-    console.log(`Your Score:${humanScore} Computer Score:${computerScore}`)
+
+    if (humanScore === 5) {
+        container.innerHTML = ""
+        let winMessage = document.createElement("h1")
+        winMessage.textContent = "Congrats, you won the game by reaching 5 points first!"
+        scores.textContent = `Your Score:${humanScore} Computer Score: ${computerScore}`
+        startBtn.textContent = "Restart"
+        container.append(scores, roundWinMessage, winMessage, startBtn)
+    } else if (computerScore === 5) {
+       container.innerHTML = ""
+        let winMessage = document.createElement("h1")
+        winMessage.textContent = "The Computer won the game by reaching 5 points first, try again!"
+        scores.textContent = `Your Score:${humanScore} Computer Score: ${computerScore}`
+        startBtn.textContent = "Restart"
+        container.append(scores, roundWinMessage, winMessage, startBtn) 
+    } else {
+    scores.textContent = `Your Score:${humanScore} Computer Score: ${computerScore}`
+    container.append(scores, roundWinMessage)
+    }
 }
 
-    for(let i = 5; i > 0; i--) {
 
-    playRound(getHumanChoice(), getComputerChoice())
 
-    }
-    (humanScore > computerScore) ? console.log(`You won with a score of ${humanScore} against ${computerScore}`) : console.log(`You lost with a score of ${humanScore} against ${computerScore}`)
+
+function getComputerChoice() {
+    computerChoice = choices[Math.floor(Math.random() * 3)]
+}
+
+function setHumanChoice() {
+    humanChoice = event.target.textContent.toLowerCase()
+}
     
 }
